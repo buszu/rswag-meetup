05d3959450a25243edafa734eedbdf727803609d
Krzysztof Buszewicz <krzysztof.buszewicz@gmail.com>
2021-09-18 19:59:41 +0200

    bebokscreation

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'constants/beboks/moods'
+require 'constants/hearts/colors'
+
+def parse_swagger_schema(name, version: 'v1')
+  JSON.parse(File.read("swagger/#{version}/schemas/#{name}.json"))
+end
 
 RSpec.configure do |config|
   config.swagger_root = Rails.root.join('swagger').to_s
@@ -18,7 +24,21 @@ RSpec.configure do |config|
           url: 'http://localhost:3000',
           description: 'Localhost'
         }
-      ]
+      ],
+      components: {
+        schemas: {
+          bebok_schema: parse_swagger_schema('bebok_schema'),
+          bebok_moods_enum: {
+            type: 'string',
+            enum: Constants::Beboks::Moods::LIST
+          },
+          heart_schema: parse_swagger_schema('heart_schema'),
+          heart_colors_enum: {
+            type: 'string',
+            enum: Constants::Hearts::Colors::LIST
+          }
+        }
+      }
     }
   }
 
