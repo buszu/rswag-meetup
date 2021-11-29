afa3df99243b4fc405794c02fa0f25514939f635
Krzysztof Buszewicz <krzysztof.buszewicz@gmail.com>
2021-11-29 14:02:59 +0100

    rubocop -A

--- spec/requests/api/v1/beboks/create_spec.rb
+++ spec/requests/api/v1/beboks/create_spec.rb
@@ -13,45 +13,45 @@ RSpec.describe 'api/v1/beboks#create', type: :request do
       produces 'application/json'
 
       parameter name: :input,
-          in: :body,
-          schema: {
-            type: 'object',
-            required: %w[name mood heart],
-            properties: {
-              name: {
-                type: 'string',
-                minLength: 1,
-                maxLength: Beboks::CreateContract::NAME_MAX_SIZE
-              },
-              mood: {
-                type: 'string',
-                enum: Beboks::CreateContract::ALLOWED_MOODS
-              },
-              heart: {
-                type: 'object',
-                required: %w[color bpm],
-                properties: {
-                  color: {
-                    type: 'string',
-                    enum: Beboks::CreateContract::HEART_COLORS
-                  },
-                  bpm: {
-                    type: 'integer',
-                    minimum: 1
+                in: :body,
+                schema: {
+                  type: 'object',
+                  required: %w[name mood heart],
+                  properties: {
+                    name: {
+                      type: 'string',
+                      minLength: 1,
+                      maxLength: Beboks::CreateContract::NAME_MAX_SIZE
+                    },
+                    mood: {
+                      type: 'string',
+                      enum: Beboks::CreateContract::ALLOWED_MOODS
+                    },
+                    heart: {
+                      type: 'object',
+                      required: %w[color bpm],
+                      properties: {
+                        color: {
+                          type: 'string',
+                          enum: Beboks::CreateContract::HEART_COLORS
+                        },
+                        bpm: {
+                          type: 'integer',
+                          minimum: 1
+                        }
+                      }
+                    }
                   }
                 }
-              }
-            }
-          }
 
       response(201, 'created') do
         schema '$ref': '#/components/schemas/bebok_schema'
 
-        let(:input) {
+        let(:input) do
           attributes_for(:bebok).slice(:name, :mood).merge(
             heart: attributes_for(:heart).slice(:color, :bpm)
           )
-        }
+        end
 
         run_test!
       end
